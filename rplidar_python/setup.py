122a6cd73b3e6822af4725fea1c864413df76c4a
joshua salzedo <jsalzedo@appliedinvention.com>
2020-01-21 22:54:58 -0800

    glance over setup.py

--- rplidar_python/setup.py
+++ rplidar_python/setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, Extension
 
-setup(name = "rplidar_python",
-      version = "0.1",
-      ext_modules = [Extension("rplidar_python", ["main.cpp"])]
-      );
+setup(name="rplidar_python",
+      version="0.1",
+        # FIXME all the necessary library bindings...
+      ext_modules=[Extension(
+          "rplidar_python",
+          sources=["bindings/main.cpp"],
+          include_dirs=["bindings/"]
+      )
+      ]
+      )
